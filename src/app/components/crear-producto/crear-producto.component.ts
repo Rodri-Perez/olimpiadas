6155687075246eb253e4e1f96539f60e3b9631e3
rodri-perez <rodri.08gabriel.perez@gmail.com>
2024-08-19 12:19:28 -0300

    19-08

--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-crear-producto',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, NgIf],
   templateUrl: './crear-producto.component.html',
   styleUrl: './crear-producto.component.css',
 })
-export class CrearProductoComponent {}
+export class CrearProductoComponent implements OnInit {
+  productoForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.productoForm = this.fb.group({
+      producto: ['', Validators.required],
+      categoria: ['', Validators.required],
+      ubicacion: ['', Validators.required],
+      precio: ['', Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  agregarProducto() {
+    console.log(this.productoForm);
+  }
+}
