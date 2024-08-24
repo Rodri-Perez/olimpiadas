5808f20f4a1b75e8b6cc3f8dbf90618f6fecaca5
rodri-perez <rodri.08gabriel.perez@gmail.com>
2024-08-24 20:26:35 -0300

    frontend

--- src/app/components/listar-productos/listar-productos.component.ts
+++ src/app/components/listar-productos/listar-productos.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CargarScriptsService } from '../../services/cargar-scripts.service';
 
 @Component({
   selector: 'app-listar-productos',
   standalone: true,
   imports: [RouterLink],
+  providers: [CargarScriptsService],
   templateUrl: './listar-productos.component.html',
   styleUrl: './listar-productos.component.css',
 })
-export class ListarProductosComponent {}
+export class ListarProductosComponent {
+  constructor(private _cargaScripts: CargarScriptsService) {
+    _cargaScripts.carga(['js/dinamic']);
+  }
+}
